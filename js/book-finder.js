29d820925121066aaee23f5611ccb24a47cb7df5
Ashley Toney <amtoney524@gmail.com>
2021-08-11 17:29:32 -0500

    Added search form with submit function

---
+++ js/book-finder.js
@@ -0,0 +1,7 @@
+function findBooks() {
+  let phraseField = document.querySelector(".phrase-field");
+  let phrase = phraseField.value;
+  console.log(phrase);
+
+  return false; // stops redirect
+}
